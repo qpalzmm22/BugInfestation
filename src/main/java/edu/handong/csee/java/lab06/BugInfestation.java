8911f5e18898f821f1747c136e1cde139ee16083
Isaac Chae <qpalzmm22@gmail.com>
2018-03-29 11:10:39 +0900

    Variable declared

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -1,8 +1,17 @@
-package edu.handong.csee.java.lab06; //sd/fsdfsdfsd
+package edu.handong.csee.java.lab06; 
 
-public class BugInfestation {	//dfkjgdlkfjglkdjgdfgasdasd
-
-	public static void main(String[] args) {	//sdfsdf
+public class BugInfestation {	
+	final double GROWTH_RATE = 0.95;
+	final double ONE_BUG_VOLUME = 0.002;
+	double houseVolume;
+	double startPopulation;
+	int countWeeks;
+	double population;
+	double totalBugVolume;
+	double newBugs;
+	double newBugVolume;
+	
+	public static void main(String[] args) {	
 		// TODO Auto-generated method stub
 	}
 
