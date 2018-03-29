35fa0e1da6269fd049c085fbea77a426ae6fda80
Isaac Chae <qpalzmm22@gmail.com>
2018-03-29 11:05:06 +0900

    dasdasdasdasd

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab06; //sd/fsdfsdfsd
 
-public class BugInfestation {	//dfkjgdlkfjglkdjgdfg
+public class BugInfestation {	//dfkjgdlkfjglkdjgdfgasdasd
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
